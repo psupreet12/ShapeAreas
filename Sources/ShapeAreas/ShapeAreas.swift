60ad7dcc2cc7c43bfabf1d925bdf73029e472aa6
Supreet <psupreet12@gmail.com>
2025-03-13 13:10:52 +0530

    calculate device specifc area

--- Sources/ShapeAreas/ShapeAreas.swift
+++ Sources/ShapeAreas/ShapeAreas.swift
@@ -1,6 +1,42 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-class ShapeAreas {
-    init() {}
+import Foundation
+import UIKit
+protocol AreaFinder {
+    func calculateArea()
 }
+
+class TraingleArea {
+    var height: Int
+    var width: Int
+    init(height: Int, width: Int) {
+        self.height = height
+        self.width = width
+    }
+    func calculateArea() -> Double {
+        return Double(0.5) * Double(height) * Double(width)
+    }
+    
+}
+class SquareArea {
+    var side: Int
+    init(side: Int) {
+        self.side = side
+    }
+    func calculateArea() -> Double {
+        return Double(side * side)
+    }
+    
+}
+public class ShapeAreaCalculator {
+    // Function to calculate the area of a square (iPhone flavour)
+    public static func calculateArea() -> (String, Double) {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return ("Triangle",TraingleArea(height: 12, width: 12).calculateArea())
+        } else {
+            return ("Square",SquareArea(side: 12).calculateArea())
+        }
+    }
+}
+
